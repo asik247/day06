75072a2c427e05e7c5c3aa7323751e897467bf7b
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-21 22:12:08 +0600

    day06 first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let main = document.getElementById("para").innerHTML="hey ima change this text"
+// comparison not data type.
+// console.log(10==='10');
+console.log(main + ' plus');
+
+let a = 10
+let b = 105
+let c = "100"
+let d = 100;
+let fist = a!=b;
+// console.log(fist);
+
+let second = d!==c;
+console.log(second);
+let third = a>=b;
+let forth = b<=a;
+console.log(forth);
+console.log(third);
+
+// if use comparison
+let age = 18;
+if(age>=18){
+    console.log('Adult');
+}else{
+    console.log('Minor');
+}
+
